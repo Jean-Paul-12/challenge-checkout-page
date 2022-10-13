b44b3ae8b294d92cb58c1eab892d363b110053e0
Jean Paul Quitian <j.guillen.0612@gmail.com>
2022-10-12 22:49:34 -0500

    feat(index): Update code

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,1 @@
-document.querySelector("#btnReduce").addEventListener("click",reduce);
-document.querySelector("#btnIncrease").addEventListener("click",increase);
-
-let i = 1;
-
-function reduce (){
-    i = i - 1; 
-    document.querySelector("#msgValue").innerHTML = i
-}
-
-function increase (){
-    i = i + 1; 
-    document.querySelector("#msgValue").innerHTML = i
-}
+import "./utils"
