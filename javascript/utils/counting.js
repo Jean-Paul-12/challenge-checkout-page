ea143d02b0953d52ca8a2517045d72e1413c2b91
Jean Paul Quitian <j.guillen.0612@gmail.com>
2022-10-31 09:36:25 -0500

    feat(counting): Create code for the functionality of the buttons, the modification of the input attribute and the validation of positive numbers

--- javascript/utils/counting.js
+++ javascript/utils/counting.js
@@ -1,31 +1,70 @@
+let buttonsIncrease = document.querySelectorAll(".btnIncrease");
+let buttonsReduce = document.querySelectorAll(".btnReduce");
+const allInputs = document.querySelectorAll("input[data-price]");
+const sumaTotal = document.querySelector("#total-price--total");
+const shipping = document.getElementById("shipping").innerText;
+const priceShipping = shipping.split("$");
 
 
 
-
-const btnsReduce = document.querySelectorAll(".btnReduce")//.addEventListener("click",reduce);
-const btnsIncrease = document.querySelectorAll(".btnIncrease")//.addEventListener("click",increase);
-
-
-function reduce (event){
-    event.target.parentNode.querySelector(".msgValue").stepDown()
+for (const button of buttonsIncrease) {
+    button.addEventListener("click", addBtnSuma );
 }
 
-function increase (event){
-    event.target.parentNode.querySelector(".msgValue").stepUp()
+for (const button of buttonsReduce) {
+    button.addEventListener("click", addBtnResta );
 }
 
-for (let i = 0; i < btnsReduce.length; i++){
-    btnsReduce[i].addEventListener("click",function(event){
-        reduce(event)
-        console.log(event.target.parentNode.querySelector(".msgValue").value)
-    });
+for (const inputElement of allInputs) {
+    inputElement.addEventListener("change", inputValidation);
 }
 
-for (let i = 0; i < btnsReduce.length; i++){
-    btnsIncrease[i].addEventListener("click",function(event){
-        increase(event)
-        console.log(event.target.parentNode.querySelector(".msgValue").value)
-    });
+
+
+function addBtnSuma (event) {
+    const idInput = event.target.dataset.input;
+    document.querySelector("#" + idInput).value = parseInt(document.querySelector("#" + idInput).value) + 1;
+    recorridoInputs();
+    
 }
+
+function addBtnResta (event) {
+    const idInput = event.target.dataset.input;
+    document.querySelector("#" + idInput).value = parseInt(document.querySelector("#" + idInput).value) - 1;
+    recorridoInputs();
+    inputValidation(idInput, event)
+}
+
+function inputValidation (idInput, eventButton ) {
+    console.log(parseInt(document.querySelector("#" + idInput).value))
+
+    if (parseInt(document.querySelector("#" + idInput).value) === 0) {
+        console.log(eventButton)
+        eventButton.target.disable = false;
+    } else {
+        eventButton.target.disable = true;
+    }
+}
+
+
+function recorridoInputs () {
+    let totalProduct = 0;
+    
+    for (const inputSelector of allInputs) {
+        let productPrice = parseFloat(inputSelector.dataset.price);
+        let productCount = inputSelector.value;
+        totalProduct = totalProduct + productCount * productPrice;
+
+    }
+    if (totalProduct === 0) {
+        totalProduct = 0;
+    } else{
+        totalProduct = parseFloat(totalProduct + parseFloat(priceShipping[1]));
+    }
+    
+    sumaTotal.innerHTML = "$" + totalProduct.toFixed(2); 
+    
+}
+
 
 
